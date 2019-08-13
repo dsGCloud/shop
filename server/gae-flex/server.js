1088c783d99a0bc47ec2c870ce177e0a51ce9410
dsgcloud <danielcloud@danielcloud-macbookpro.roam.corp.google.com>
2019-08-13 16:04:25 -0500

    gae flex

--- server/gae-flex/server.js
+++
@@ -1,8 +0,0 @@
-const prpl = require('prpl-server');
-const express = require('express');
-const app = express()
-const port = 8080
-
-app.get('/*', prpl.makeHandler('./build', require('./build/polymer.json')));
-
-app.listen(port, () => console.log(`Server listening on  ${port}!`))
