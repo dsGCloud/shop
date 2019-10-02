02d060fa6ea687427461cde33e0c1acd15ae4cd3
danielcloud <dsGCloud@users.noreply.github.com>
2019-10-02 14:34:15 -0500

    new runtime

--- src/shop-home.js
+++ src/shop-home.js
@@ -122,7 +122,7 @@ class ShopHome extends PolymerElement {
   _visibleChanged(visible) {
     if (visible) {
       this.dispatchEvent(new CustomEvent('change-section', {
-        bubbles: true, composed: true, detail: {title: 'Cloud Run Club'}}));
+        bubbles: true, composed: true, detail: {title: 'Cloud Run CDMX'}}));
     }
   }
 }
